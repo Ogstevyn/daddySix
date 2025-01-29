d51f71ee4f3c9346e9dfef63274d9c64e7a5ca92
ogstevyn <secondsteveoyebamiji@gmail.com>
2025-01-29 16:54:22 +0100

    nav bar updated

--- components/customNavbar/CustomNavbar.jsx
+++ components/customNavbar/CustomNavbar.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 import "./customNavbar.css";
+// import logo from "../../public/images/daddysixlogo.png";
 const CustomNavbar = () => {
-  return <div>CustomNavbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Image
+          src="/images/daddysixlogo.png"
+          alt="Daddy6ix Academy Ltd"
+          className="logo"
+          width={150}
+          height={50}
+        />
+        <ul className="nav-links">
+          {["Home", "Mentorship", "Signals", "Contact", "FAQs"].map(
+            (link, index) => (
+              <li key={index}>
+                <Link href={`/${link.toLowerCase()}`}>
+                  {link} <span className="arrow">↗</span>
+                </Link>
+              </li>
+            )
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default CustomNavbar;
