f345fcf857546a8f979e41c0c42e64a65d4228e1
obadea <obadeagbenga@gmail.com>
2025-01-29 12:38:14 +0100

    worked om file structure

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,17 +1,17 @@
 "use client";
 
 import CustomNavbar from "@/components/customNavbar/CustomNavbar";
-import { HeroUIProvider } from "@heroui/react";
+// import { HeroUIProvider } from "@heroui/react";
 
 export default function Layout({ children }) {
   return (
-    <HeroUIProvider>
-      <div className="max-w-[1700px] mx-auto bg-customNavBg">
-        {/* Navbar */}
-        <CustomNavbar />
-        {/* Main Content */}
-        <main className="">{children}</main>
-      </div>
-    </HeroUIProvider>
+    // <HeroUIProvider>
+    <div className="max-w-[1700px] mx-auto bg-customNavBg">
+      {/* Navbar */}
+      <CustomNavbar />
+      {/* Main Content */}
+      <main className="">{children}</main>
+    </div>
+    // </HeroUIProvider>
   );
 }
